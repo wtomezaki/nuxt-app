5f58eeb7659b3ecf3510bb2639a81716453d510a
Willian Tomezaki <willian.tomezaki@sap.com>
2021-06-01 11:13:02 -0300

    wip: card structure

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,31 @@
 <template>
-  <div>
-    <input v-model="query" type="search" autocomplete="off" />
+  <div class="body">
+    <div class="container">
+      <div class="searchContainer">
+        <input
+          v-model="query"
+          class="searchInput"
+          type="search"
+          autocomplete="off"
+        />
+      </div>
 
-    <ul v-if="users.length">
-      <li v-for="user of users" :key="user.email">
-        <div>{{ user.name }}</div>
-        <div>{{ user.email }}</div>
-        <div>{{ user.title }}</div>
-      </li>
-    </ul>
+      <div v-if="users.length" class="userList">
+        <div v-for="user of users" :key="user.email" class="card">
+          <img :src="user.avatar" class="cardAvatar" />
+          <div class="cardContent"></div>
+          <div class="cardTitle">
+            <h2>{{ user.name }}</h2>
+          </div>
+          <div class="cardEmail">
+            <h4>{{ user.email }}</h4>
+          </div>
+          <div class="cardSubtitle">
+            <h4>{{ user.title }}</h4>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -25,44 +42,67 @@ export default Vue.extend({
   },
   watch: {
     async query(query) {
-      this.users = await this.$content('').limit(10).search(query).fetch()
+      this.users = await this.$content('').limit(20).search(query).fetch()
     },
   },
   async mounted(): Promise<void> {
-    this.users = await this.$content('').limit(10).fetch()
+    this.users = await this.$content('').limit(20).fetch()
   },
 })
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
+.body {
+  background-color: aliceblue;
+  padding: 10px 0;
   min-height: 100vh;
+  width: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  text-align: center;
+}
+.container {
+  background-color: white;
+  padding: 16px 32px;
+  height: 80vh;
+  width: 40%;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.searchContainer {
+  width: 100%;
+  position: relative;
+  top: 0;
+  height: 5%;
+  margin: 10px 0;
+}
+.searchInput {
+  width: 100%;
+  height: 100%;
+  font-size: 120%;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.userList {
+  height: 95%;
+  overflow-y: auto;
 }
 
-.links {
-  padding-top: 15px;
+.card {
+  height: 120px;
+  margin: 15px 0;
+  display: flex;
+  flex-direction: row;
+  background-color: rgba(211, 211, 211, 0.5);
+}
+
+.cardAvatar {
+  width: 20%;
+  background-color: lightgray;
+  object-fit: contain;
+}
+
+.cardContent {
+  /* width: 80%; */
+  padding: 15px 10px;
 }
 </style>
