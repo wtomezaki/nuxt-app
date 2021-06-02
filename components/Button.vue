a39389479e43a25b553a027da71248fe182dc795
Willian Tomezaki <willian.tomezaki@sap.com>
2021-06-02 17:48:15 -0300

    create button component

---
+++ components/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="button" @click="$emit('click', undefined)">
+    {{ text.toUpperCase() }}
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Button',
+  props: {
+    text: { type: String, required: true },
+  },
+})
+</script>
+
+<style>
+.button {
+  padding: 5px 10px;
+  border: none;
+  max-width: 64px;
+  background-color: transparent;
+  color: #1fa296;
+  font-size: 1.2em;
+  font-weight: bold;
+  text-align: center;
+}
+</style>
